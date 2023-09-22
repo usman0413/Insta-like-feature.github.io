355adaa7a4e8063829e6c0f01da42f85e8950b3e
MUHAMMAD USMAN ARIF <uarif6851@gmail.com>
2023-09-22 18:52:47 +0500

    instagram

---
+++ index.js
@@ -0,0 +1,18 @@
+var iui = document.querySelector("#container")
+var love = document.querySelector("i")
+
+iui.addEventListener("dblclick"  , function(){
+love.style.transform = "translate(-50%, -50%) scale(1)"
+love.style.opacity = 0.8
+
+setTimeout(function(){
+    love.style.opacity = 0
+
+},1000)
+
+setTimeout(function(){
+    love.style.transform = "translate(-50%, -50%) scale(0)"
+
+},2000)
+
+})
